f2cdf7cc0177119f1ed1f1c85f1681f1899994a2
MiguelX413 <lbpgaming27@gmail.com>
2023-04-15 03:15:08 +0000

    Display can frames to console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use socketcan::{CanSocket, Socket};
+
 fn main() {
-    println!("Hello, world!");
+    let can_socket = CanSocket::open("can0").unwrap();
+    loop {
+        if let Ok(frame) = can_socket.read_frame() {
+            println!("{:?}", frame);
+        }
+    }
 }
